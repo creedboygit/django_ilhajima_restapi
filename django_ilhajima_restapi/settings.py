bd04600e563d17a5128e0cda9d94f256757f3841
creedboy <polozhzh@gmail.com>
2024-07-12 18:20:52 +0900

    django rest framework init

--- django_ilhajima_restapi/settings.py
+++ django_ilhajima_restapi/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "restapi.apps.RestapiConfig"
+    "restapi.apps.RestapiConfig",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
